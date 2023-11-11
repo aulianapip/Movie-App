665158946bea12cfd362dd8af057a91a3866e93f
aulia.afif <aulia.afif@iconpln.co.id>
2023-11-11 09:46:01 +0700

    update setting activity from theme to change language

--- app/src/main/java/com/aulia/movieapp/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/aulia/movieapp/ui/setting/SettingActivity.kt
@@ -1,42 +1,24 @@
 package com.aulia.movieapp.ui.setting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CompoundButton
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.ViewModelProvider
-import com.aulia.core.utils.sharepref.SettingPreferences
-import com.aulia.core.utils.sharepref.dataStore
+import android.provider.Settings
 import com.aulia.movieapp.databinding.ActivitySettingBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val pref = SettingPreferences.getInstance(application.dataStore)
-        val settingViewModel = ViewModelProvider(this, ViewModelSettingFactory(pref)).get(
-            SettingViewModel::class.java
-        )
-        settingViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
-            if (isDarkModeActive) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                binding.switchTheme.isChecked = true
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                binding.switchTheme.isChecked = false
-            }
-        }
-
-        binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
-            settingViewModel.saveThemeSetting(isChecked)
+        binding.btnChange.setOnClickListener {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
         }
     }
 
